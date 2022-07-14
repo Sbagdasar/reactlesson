ce10e56b9450b559db57d06b270b3fb715bdd700
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-14 21:10:08 +0300

    added Product.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import {Header} from "./Header";
 import {BestSellers} from "./BestSellers";
+import {
+    Routes,
+    Route,
+} from "react-router-dom";
+import {Product} from "./Product";
 
 function App() {
   return (
     <div className='appContainer'>
       <Header/>
-        <BestSellers/>
+        <hr className="divider" />
+        <Routes>
+            <Route path="/" element={<BestSellers />} />
+            <Route path="product" element={<Product />} />
+        </Routes>
     </div>
   );
 }
